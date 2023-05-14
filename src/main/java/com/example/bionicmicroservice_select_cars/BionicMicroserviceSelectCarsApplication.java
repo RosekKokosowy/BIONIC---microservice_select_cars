e7053e59db98fcabe2169c0c1e5ccfb45080ee8c
Mateusz Cybulski <235847@edu.p.lodz.pl>
2023-05-14 15:00:57 +0200

    Init

---
+++ src/main/java/com/example/bionicmicroservice_select_cars/BionicMicroserviceSelectCarsApplication.java
@@ -0,0 +1,13 @@
+package com.example.bionicmicroservice_select_cars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BionicMicroserviceSelectCarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BionicMicroserviceSelectCarsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bionicmicroservice_select_cars/BionicMicroserviceSelectCarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bionicmicroservice_select_cars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BionicMicroserviceSelectCarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
